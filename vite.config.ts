d7983f6627149e0ff23a1776f3deaa221eb55142
Ryan Montville <ryan.a.montville@gmail.com>
2025-12-29 14:06:55 -0500

    removing donate and distribute log pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         createNewEvent: resolve(__dirname, "create-new-event.html"),
-        distributedInventory: resolve(__dirname, "distributed-inventory.html"),
         donate: resolve(__dirname, "donate.html"),
-        donatedInventory: resolve(__dirname, "donated-inventory.html"),
         eventSignUp: resolve(__dirname, "event-sign-up.html"),
         events: resolve(__dirname, "events.html"),
         inventory: resolve(__dirname, "inventory.html"),
